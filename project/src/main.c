a7444f449bae3f2f1b7b5d7ebb309f2183131f93
Matvey Popov <matvey.popov19@mail.ru>
2021-11-08 13:34:13 +0300

    1

--- project/src/main.c
+++ project/src/main.c
@@ -6,11 +6,17 @@ int main(int argc, const char **argv) {
     if (argc != 2) {
         return -1;
     }
-
+    printf("Hello, world!");
     const char *path_to_eml = argv[1];
     puts(path_to_eml);
 
-    //1
+    char st1[100];
+    FILE *fin;
+    fin=fopen("btests/emails/8bitmime.eml", "r");
+    if ( NULL != fgets ( st1, 100, fin ) ){
+        // выводим строку на экран
+        printf("%s ",st1);
+        }  
 
     return 0;
 }
